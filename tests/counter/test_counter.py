b50e9dc3253feaa6cf54b535f9c99e4c52214881
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2023-04-27 16:32:24 -0300

    feat: test counter implemenentado

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    result = count_ocurrences('data/jobs.csv', 'Python')
+    assert result == 1639
+    result2 = count_ocurrences('data/jobs.csv', 'python')
+    assert result2 == 1639

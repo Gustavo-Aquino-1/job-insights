c9c62fa713f08b4d10465bcf34387f46015dbd4c
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2023-04-27 15:07:47 -0300

    feat: get unique jobs implementado

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -37,7 +37,9 @@ def get_unique_job_types(path: str) -> List[str]:
     list
         List of unique job types
     """
-    raise NotImplementedError
+    content = read(path)
+    types = [x['job_type'] for x in content]
+    return set(types)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
